98140dde3a56eaea51c839ad34d2ae65417f5990
crystall.101600 <crystall.101600@gmail.com>
2025-05-26 08:49:33 +0000

    Corresponding .g file generated by HiveType Adaptor for material model.

---
+++ lib/data/models/material_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'material_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MaterialModelAdapter extends TypeAdapter<MaterialModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  MaterialModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return MaterialModel(
+      name: fields[0] as String,
+      currentQuantity: fields[1] as double,
+      thresholdQuantity: fields[2] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, MaterialModel obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.currentQuantity)
+      ..writeByte(2)
+      ..write(obj.thresholdQuantity);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MaterialModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
