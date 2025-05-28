0a432e1087979748790969fac583c4ed1c07e575
crystall.101600 <crystall.101600@gmail.com>
2025-05-28 10:59:37 +0000

    Added different methods for the integration with google sheets.

--- lib/providers/inventory_cubit.dart
+++ lib/providers/inventory_cubit.dart
@@ -1,38 +1,75 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:materialapp/data/models/composition_model.dart';
 import 'package:materialapp/data/models/material_model.dart';
+import 'package:materialapp/data/services/google_service.dart';
 import 'package:materialapp/data/services/hive_service.dart';
 
 class InventoryCubit extends Cubit<List<MaterialModel>> {
-  InventoryCubit(this.hiveService) : super([]) {
+  InventoryCubit(this.hiveService, this.googleService) : super([]) {
     loadMaterials();
   }
+
   final HiveService hiveService;
+  final GoogleSheetsService googleService;
 
-  void loadMaterials() {
+  Future<void> loadMaterials() async {
     emit(hiveService.getAllMaterials());
+
+    try {
+      final remoteMaterials = await googleService.fetchMaterials();
+      for (final material in remoteMaterials) {
+        hiveService.addMaterial(material);
+      }
+      emit(hiveService.getAllMaterials());
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error fetching remote materials: $e');
+      }
+    }
   }
 
-  void addMaterial(MaterialModel material) {
+  Future<void> addMaterial(MaterialModel material) async {
     hiveService.addMaterial(material);
-    loadMaterials();
+    try {
+      await googleService.upsertMaterial(material); 
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error syncing with Google Sheets on add: $e');
+      }
+    }
+    await loadMaterials();
   }
 
-  void deleteMaterial(String name) {
+  Future<void> deleteMaterial(String name) async {
     hiveService.deleteMaterial(name);
-    loadMaterials();
+    try {
+      await googleService.deleteMaterial(name); 
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error syncing with Google Sheets on delete: $e');
+      }
+    }
+    await loadMaterials();
   }
 
-  void updateMaterial(String name, MaterialModel updated) {
+  Future<void> updateMaterial(String name, MaterialModel updated) async {
     hiveService.updateMaterial(name, updated);
-    loadMaterials();
+    try {
+      await googleService.upsertMaterial(updated); 
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error syncing with Google Sheets on update: $e');
+      }
+    }
+    await loadMaterials();
   }
 
-  void manufactureProduct(
+  Future<void> manufactureProduct(
     String productName,
     int quantity,
     List<CompositionModel> compositions,
-  ) {
+  ) async {
     final productComposition = compositions.firstWhere(
       (comp) => comp.productName == productName,
       orElse: () => throw Exception('Composition not found'),
@@ -49,14 +86,18 @@ class InventoryCubit extends Cubit<List<MaterialModel>> {
         (mat) => mat.name == materialName,
         orElse: () => throw Exception('Material not found'),
       );
-
       final updatedMaterial = material.copyWith(
         currentQuantity: material.currentQuantity - totalRequired,
       );
-
       hiveService.updateMaterial(materialName, updatedMaterial);
+      try {
+        await googleService.upsertMaterial(updatedMaterial); 
+      } catch (e) {
+        if (kDebugMode) {
+          print('Error syncing with Google Sheets on manufacture: $e');
+        }
+      }
     }
-
-    loadMaterials();
+    await loadMaterials();
   }
 }
