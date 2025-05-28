1c630eb01c135f993e67954078f72e0c08d540b8
crystall.101600 <crystall.101600@gmail.com>
2025-05-28 10:56:01 +0000

    Added the route to log page and necessary parameters in the other pages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:materialapp/data/models/material_model.dart';
 import 'package:materialapp/data/models/composition_model.dart';
+import 'package:materialapp/data/models/material_model.dart';
+import 'package:materialapp/data/services/google_service.dart';
 import 'package:materialapp/data/services/hive_service.dart';
 import 'package:materialapp/providers/composition_cubit.dart';
 import 'package:materialapp/providers/inventory_cubit.dart';
+import 'package:materialapp/screens/inventory_page.dart';
+import 'package:materialapp/screens/log_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
 
-  Hive..registerAdapter(MaterialModelAdapter())
-  ..registerAdapter(CompositionModelAdapter());
+  Hive
+    ..registerAdapter(MaterialModelAdapter())
+    ..registerAdapter(CompositionModelAdapter());
 
   await Hive.openBox<MaterialModel>('inventory');
   await Hive.openBox<CompositionModel>('composition');
 
-  final hiveService = HiveService(); 
+  final hiveService = HiveService();
+  final googleService = GoogleSheetsService();
 
   runApp(
     MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => InventoryCubit(hiveService)),
-        BlocProvider(create: (_) => CompositionCubit(hiveService)),
+        BlocProvider(create: (_) => InventoryCubit(
+          hiveService, googleService,
+        ),
+      ),
+        BlocProvider(create: (_) => CompositionCubit(
+          hiveService,
+        ),
+      ),
       ],
       child: const RawMaterialApp(),
     ),
@@ -35,15 +46,12 @@ class RawMaterialApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Raw Material App',
-          ),
-        ),
-      ), // Placeholder
+      home: const InventoryPage(),
+      routes: {
+        '/log': (_) => const ManufacturingLogPage(),
+      },
     );
   }
 }
