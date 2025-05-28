c493d7891a9417ba66bd792de1bc169d6e77c8f5
crystall.101600 <crystall.101600@gmail.com>
2025-05-28 11:02:31 +0000

    Stores development configurations.

---
+++ lib/util/config.dart
@@ -0,0 +1,4 @@
+class Config {
+  static const String scriptUrl = 'https://script.google.com/macros/s/AKfycbzw6os2B5aIN2lSQzu_RZv-9IdNH43tSv3XDbRJkKL373unkbcqU2mntqmoAWmGzTM7Dw/exec';
+  static const String apiKey = '1266_gL@#';
+}
