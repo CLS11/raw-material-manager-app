a5a5a6560d373a640acdc4acf9eac243b3486e2f
crystall.101600 <crystall.101600@gmail.com>
2025-05-28 10:56:41 +0000

    Added two methods to help in data transfer to the google sheets.

--- lib/data/models/composition_model.dart
+++ lib/data/models/composition_model.dart
@@ -9,9 +9,31 @@ class CompositionModel {
     required this.productName,
     required this.materialQuantities,
   });
+
+  factory CompositionModel.fromJson(Map<String, dynamic> json) {
+    return CompositionModel(
+      productName: json['productName'] as String,
+      materialQuantities: Map<String, double>.from(
+        (json['materialQuantities'] as Map).map(
+          (key, value) => MapEntry(
+            key.toString(), 
+            double.parse(value.toString()),
+          ),
+        ),
+      ),
+    );
+  }
+
   @HiveField(0)
   final String productName;
 
   @HiveField(1)
   final Map<String, double> materialQuantities;
+
+   Map<String, dynamic> toJson() {
+    return {
+      'productName': productName,
+      'materialQuantities': materialQuantities,
+    };
+  }
 }
